aa0f54a6318344d1f034a74b9f79655026862995
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-02-02 20:07:41 -0500

    week 2 assignment

---
+++ week-2/assignment/index.js
@@ -0,0 +1,20 @@
+var newH1 = document.createElement('h1');
+newH1.textContent = 'Welcome to my JS site';
+document.body.appendChild(newH1);
+
+var newP = document.createElement('p');
+newP.textContent = 'All of this was created with Javascript';
+document.body.appendChild(newP);
+
+
+var newListItem = document.createElement('ol');
+newListItem.textContent = 'Vangaurd';
+document.body.appendChild(newListItem);
+
+var newListItem = document.createElement('ol');
+newListItem.textContent = 'Warzone';
+document.body.appendChild(newListItem);
+
+var newListItem = document.createElement('ol');
+newListItem.textContent = 'Kingdom Hearts';
+document.body.appendChild(newListItem);
