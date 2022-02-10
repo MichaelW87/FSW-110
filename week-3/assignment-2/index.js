ccb1fdaa9846ec47e2d256e90c5defed096531af
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-02-10 17:19:36 -0500

    assignment-2

---
+++ week-3/assignment-2/index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 5; i++) {
+    var gameName = document.createElement('h2');
+    gameName.textContent = 'SqueezedComa';
+    document.body.appendChild(gameName);
+    gameName.style.fontSize = '20px';
+    gameName.style.fontWeight = 'lighter';
+    gameName.style.fontFamily = 'sansSerif'
+    gameName.style.color = 'cornFlowerBlue';
+}
+
+gameName.border.add();
